8c08e5cf66625fe29af0bfc99c565ab0f6d647d2
pradeepkrishnarao <36490187+pradeepkrishnarao@users.noreply.github.com>
2018-05-18 07:06:11 -0400

    Add files via upload

---
+++ assignment7.2.js
@@ -0,0 +1,15 @@
+var valFor=0;//valFor is initialized to 0
+console.log("Using for loop");
+//for loop executes i to 10 iterations until the valFor is equal to 50
+for(i=0;valFor<=50;i++){
+    valFor=valFor+5;
+    console.log(valFor);
+}
+var j,valWhile=0;//j and valWhile is initialized to 0
+console.log("Using while loop");
+//while loop executes j to 10 iterations until the valWhile is equal to 50
+while(valWhile<=50){
+    j++;
+    valWhile=valWhile+5;
+    console.log(valWhile);
+}
